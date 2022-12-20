e404a039f3294aac033b97dc5cbae6eb073c3f99
Jonathanbir <jonathan79111@gmail.com>
2022-12-20 23:11:36 +0800

    commit: first commit

---
+++ src/App.vue
@@ -0,0 +1,10 @@
+<template>
+  <TheIconVue icon="home" />
+</template>
+
+<script setup>
+import "./assets/base.css";
+import TheIconVue from "./components/TheIcon.vue";
+</script>
+
+<style lang="scss" scoped></style>
